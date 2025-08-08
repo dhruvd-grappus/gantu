bc4b050c0175a6ea7711498f68f69da12e53ff72
dhruvd-grappus <dhruv.devasthale@grappus.com>
2025-08-08 20:22:37 +0530

    Initial commit

---
+++ app/index.tsx
@@ -0,0 +1,16 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      className=""
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text className="md:text-black text-red-500 text-2xl">Edit app/index.tsx to edit this screen.</Text>
+    </View>
+  );
+}
